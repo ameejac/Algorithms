f86bbaf1090e07d8c336b42745748aaa89ef0bd9
AlekyaC <alekya.cherukoori@dreamorbit.com>
2021-10-18 17:40:29 +0530

    [added: Insert the elements and sort them in order]

---
+++ InsertionSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    class InsertionSort
+    {
+        //this method is to Insert the elements and sort them in order.
+        public void InsertionSortOrder()
+        {
+            Console.WriteLine("enter the size of an array:");
+            //Convert string into Integer value and store it in size.
+            int size = Convert.ToInt32(Console.ReadLine());
+            //Created an array to insert the elements
+            string[] array = new string[size];
+            Console.WriteLine("enter the string elements of array:");
+            for (int i = 0; i < size; i++)
+            {
+                //elements will store in index(position) 0 and 1
+                array[i] = Console.ReadLine();
+            }
+            for (int i = 1; i < array.Length; i++)
+            {
+                //array[i] element will store in key.
+                string key = array[i];
+
+                int j = i - 1;
+
+
+                while ((j >= 0) && (array[j].CompareTo(key) > 0))
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                //key stores in array
+                array[j + 1] = key;
+            }
+
+            //giving elements after sorted.
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AlgorithmProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //created an object with class name InsertionSort.
+            InsertionSort insertion = new InsertionSort();
+            insertion.InsertionSortOrder();
+            Console.ReadLine();
+
+        }
+    }
+}
